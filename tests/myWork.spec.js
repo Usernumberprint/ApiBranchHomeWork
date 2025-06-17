95ec5a09d5a84fd9fe33861403ece5abd6ca6ae3
Usernumberprint <flash.morozof@gmail.com>
2025-06-17 23:26:37 +0300

    два токена отправляется

--- tests/myWork.spec.js
+++ tests/myWork.spec.js
@@ -8,26 +8,33 @@ let token;
 
 test.describe('Challenge', () => {
 
-    /*test.beforeAll(async ({request}) => {
-      const response = await request.post(`${URL}challenger`);
-    });*/
-    test('Challenges', async ({request}) => {
+   test.beforeAll('@post Challenger', async ({request}) => {
       const response = await request.post(`${URL}challenger`);
       const headers =  await response.headers();
       token = headers['x-challenger']; //токен равен значения x-challenger из запроса post - response
       console.log(token);
-      expect(response.status()).toBe(200);
+      //expect(response.status()).toBe(200);
 });
- test('Challenge', async ({request}) => {
+ test('@get Challenges', async ({request}) => {
       const response = await request.get(`${URL}challenges`, {headers : {
-        'x-challenger' : token,
+        'x-challenger' : token
       }});
-      
       const body = await response.json(); //преобразовываем запрос response в формат json
       expect(body.challenges.length).toBe(59);
       console.log(body)
+ 
       
 });
 
+test('@get todos ', async ({request}) => {
+      const response = await request.get(`${URL}todos `, {headers : {
+        'x-challenger' : token
+      }});
+      
+      const body = await response.json(); //преобразовываем запрос response в формат json
+      expect(body.todos.length).toBe(10);
+      console.log(body)
+
+});
 
 });
