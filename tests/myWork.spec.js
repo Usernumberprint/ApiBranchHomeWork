48101828a7be9954e724c14b1fdfac61e55c73ee
Usernumberprint <flash.morozof@gmail.com>
2025-06-21 16:06:34 +0300

    getы готовы

--- tests/myWork.spec.js
+++ tests/myWork.spec.js
@@ -18,7 +18,7 @@ test.describe('Challenge', () => {
       console.log(token);
       //expect(response.status()).toBe(200);
 });
- test('@get Challenges', async ({request}) => {
+ test('@1. get Challenges', async ({request}) => {
       const response = await request.get(`${URL}challenges`);
       const body = await response.json(); //преобразовываем запрос response в формат json
       expect(body.challenges.length).toBe(59);
@@ -27,7 +27,7 @@ test.describe('Challenge', () => {
       
 });
 
-test('@get todos ', async ({request}) => {
+test('@2. get todos1 ', async ({request}) => {
       const response = await request.get(`${URL}todos`);
       
       const body = await response.json(); //преобразовываем запрос response в формат json
@@ -37,34 +37,59 @@ test('@get todos ', async ({request}) => {
 });
 
 
-test('@get Todo', async ({request}) => {
+test('@3. get Todo', async ({request}) => {
   const response = await request.get(`${URL}todo`);
   //const headers = await response.json();
   expect(response.status()).toBe(404);
   
 })
 
-test('@get Todos', async ({request}) => {
+test('@4. get todos2', async ({request}) => {
   const response = await request.get(`${URL}todos`, {headers : {
     'x-challenger' : token,
   }});
   const headers = await response.json();
-  console.l
   console.log(headers.todos);
 
   expect(headers.todos.length).toBe(10);
-  //console.log(headers.todos.doneStatus[0]);
-  // expect(headers.todos.doneStatus[3] === false) {
-    if (headers.todos.doneStatus === false) {
-    console.log('Статус FALSE')
-  } else {
-    console.log('СТАТУС TRUE')
-  }
-  if (response.status() === 200) {
+  console.log('Все верно, всего 10 id')
+ 
+});
+test('@5. get todos3', async ({request}) => {
+  const response = await request.get(`${URL}todos`, {headers : {
+    'x-challenger' : token,
+  }});
+  const headers = await response.json();
+  console.log(headers.todos);
+ if (response.status() === 200) {
     console.log('Да, статус норм')
   } else {
     console.log('Статус не 200')
-  }
+  };
+
+});
+test('@6. get todos4', async ({request}) => {
+  const response = await request.get(`${URL}todos`, {headers : {
+    'x-challenger' : token,
+  }});
+  const headers = await response.json();
+  const a= headers.todos[0];
+  console.log(headers.todos[0]);
+
 });
 
+test('@7.GET /todos (200)', async ({ request }) => {
+  const response = await request.get(`${baseURL}/todos`);
+  expect(response.status()).toBe(200);
+
+  const data = await response.json();
+  console.log('Todos:', data);
+ 
+  expect(Array.isArray(data)).toBeTruthy();
+});
+
+test('@8.GET /todos/9999 (not exist)', async ({ request }) => {
+  const response = await request.get(`${baseURL}/todos/9999`);
+  expect(response.status()).toBe(404);
+});
 });
